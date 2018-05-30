64f6c3da5e3b7895538d198bd886fc8078e3796b
Bertrand Mermet <bertrand.mermet@datadoghq.com>
2018-05-30 14:37:28 +0200

    Revert "Remove restriction to top-level spans"
    This reverts commit 7a351c5adf0210bf2e2c6e2d440f860952a8cacb.

--- cmd/trace-agent/transaction_sampler.go
+++ cmd/trace-agent/transaction_sampler.go
@@ -63,6 +63,10 @@ func (s *transactionSampler) Add(t processedTrace) {
 }
 
 func (s *transactionSampler) shouldAnalyze(span *model.WeightedSpan) bool {
+	if !span.TopLevel {
+		return false
+	}
+
 	if operations, ok := s.analyzedSpansByService[span.Service]; ok {
 		if analyzeRate, ok := operations[span.Name]; ok {
 			if sampler.SampleByRate(span.TraceID, analyzeRate) {
