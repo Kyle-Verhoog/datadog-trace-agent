5b5cb24bddec25d20165a0d08d95151768b00018
Matthieu Vlad Hauglustaine <vlad@datadoghq.com>
2018-06-04 17:32:38 +0200

    config: add max_entries_per_payload config value.

--- writer/config/stats_writer.go
+++ writer/config/stats_writer.go
@@ -4,14 +4,23 @@ import "time"
 
 // StatsWriterConfig contains the configuration to customize the behaviour of a TraceWriter.
 type StatsWriterConfig struct {
-	UpdateInfoPeriod time.Duration
-	SenderConfig     QueuablePayloadSenderConf
+	MaxEntriesPerPayload int
+	UpdateInfoPeriod     time.Duration
+	SenderConfig         QueuablePayloadSenderConf
 }
 
-// DefaultStatsWriterConfig creates a new instance of a StatsWriterConfig using default values.
+// DefaultStatsWriterConfig creates a new instance of a
+// StatsWriterConfig using default values.
 func DefaultStatsWriterConfig() StatsWriterConfig {
+	// An entry has an average size of 125 bytes in a compressed
+	// payload. The current Datadog intake API limits a compressed
+	// payload to ~3MB (24,000 entries), but let's have the
+	// default ensure we don't have paylods > 1.5 MB (12,000
+	// entries).
+
 	return StatsWriterConfig{
-		UpdateInfoPeriod: 1 * time.Minute,
-		SenderConfig:     DefaultQueuablePayloadSenderConf(),
+		MaxEntriesPerPayload: 12000,
+		UpdateInfoPeriod:     1 * time.Minute,
+		SenderConfig:         DefaultQueuablePayloadSenderConf(),
 	}
 }
